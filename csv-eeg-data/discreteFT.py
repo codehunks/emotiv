9d0e92474ae1cebb3ccc7b6fa0a46f972a97ac2f
Shivam Bansal <shivam5992@gmail.com>
2013-09-12 11:16:09 -0700

    File optimized

--- csv-eeg-data/discreteFT.py
+++ csv-eeg-data/discreteFT.py
@@ -1,3 +1,6 @@
+##utility file to process the eeg data received in the form of csv format
+##  This file calculated discrete fourier transform of the data
+
 import csv
 import math
 import cmath
@@ -14,12 +17,14 @@ with open(file_name, 'rt') as cfile:
         a_list.append( row[0].split(','))
         row_count += 1
 
+    #define the value of 'm' as the number of columns in the csv file
     m = 22
     n = row_count
 
     x = [0]*m
     k = -1
-    # K : deviation factor
+    # K : deviation factor, by default equal to -1
+    print ("Discrete Fourier transform in the order of colums:")
     for j in range(0,m):
         for i in range(1,n):
             x[j] = x[j] + float(a_list[i][j])*cmath.exp(complex(0.0,-2*math.pi*i*k/n))
