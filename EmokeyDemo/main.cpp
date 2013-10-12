679ccb1c71cce868d1d9c3a60088b2dd179b2968
Varun Malhotra <varun2902@gmail.com>
2013-10-13 01:12:49 +0530

    four cognitive actions included

--- EmokeyDemo/main.cpp
+++ EmokeyDemo/main.cpp
@@ -6,17 +6,34 @@
 #include<iostream>
 #include<fstream>
 #include<conio.h>
+#include<process.h>
 
 #include<string.h>
 
 using namespace std;
 char output[100];
 
-int x=0,y=0,check=0;
+int x=0,y=0,z=1,ctr=0,check=0;
+
+void blinkCursor() {
+if(z==1)
+	glColor3f(1.0,0.0,0.0);
+else
+	glColor3f(0.0,0.0,0.0);
+
+glBegin(GL_POLYGON);
+	    glVertex2d(0+x,0+y);
+    	glVertex2d(200+x,0+y);
+    	glVertex2d(200+x,200+y);
+    	glVertex2d(0+x,200+y);
+    glEnd();
+	glFlush();
+
+}
 
 void display(void){
 
-	glClearColor(0.0,0.0,0.0,0.0);
+	/*glClearColor(0.0,0.0,0.0,0.0);
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	glLineWidth(2);
@@ -34,75 +51,57 @@ void display(void){
     	glVertex2d(200,200);
     	glVertex2d(100,200);
     glEnd();
-    glFlush();
-	
-	/*ifstream fp ("log.txt", ios::in);;
-	
-	if(fp.is_open()) {
-		while(!fp.eof()) {
-			fp>> output;
-			if(strcmp(output,"D")==0)
-				y+=120;
-			if(strcmp(output,"L")==0)
-				x-=120;
-			if(strcmp(output,"R")==0)
-				x+=120;
-			if(strcmp(output,"U")==0)
-				y-=120;
-		}
-	}
-	else 
-		cout<<"error";
-	fp.close();*/
+    glFlush();*/
+	glClearColor(0.0,0.0,0.0,0.0);
+	glClear(GL_COLOR_BUFFER_BIT);
 
-	
+	glLineWidth(2);
 
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	glOrtho(0, 600, 0, 600, -1, 1);
+
+	blinkCursor();
 	
 }
 
-void func_done()
-{
-	if(_kbhit() == 1)
-	{
-	}
-	else
-	{
-	check = _kbhit();
-	if(check==1)
-	{
-		cout<<"Done\n";
-		_khbit() = 0;
-	}
-	}
-	
-	
-}
+
 void updateScreen(void)
 {	
-	/*glBegin(GL_POLYGON);
-	    glVertex2d(100+x,100+y);
-    	glVertex2d(200+x,100+y);
-    	glVertex2d(200+x,200+y);
-    	glVertex2d(100+x,200+y);
-    glEnd();
-    glFlush();
-
-	glutPostRedisplay();*/
-	
-	/*if(_kbhit())
-	{
-		cout<<"Done";
-	}*/
-	 
-	//glutPostRedisplay();
-	check = _kbhit();
-	if(check==1)
-	{
-		cout<<"Done\n";
-		_khbit() = 0;
+	char c;
+	ctr++;
+	if(ctr%150==0){
+		ctr=0;
+		z*=-1;
 	}
-	//func_done();
-		glutPostRedisplay();
+		if(_kbhit())
+		{
+			c = getch();
+			cout<<c<<endl;
+			if( c == 'd' || c == 'D')
+			 {
+				 x+= 200;
+			 }
+			 if( c == 'a' || c == 'A')
+			 {
+				 x-= 200;
+			 }
+			 if( c == 'w' || c == 'W')
+			 {
+				 y+= 200;
+			 }
+			 if( c == 's' || c == 'S')
+			 {
+				 y-= 200;
+			 }
+			 if( c == 'X' || c == 'x')
+			 {
+				 exit(0);
+			 }
+	}
+
+	glutPostRedisplay();	
 }
 
 void reshape(int w, int h)
